767d57aafd3c5a7891bf209d9ea438baaa6d7d12
e-chesoni <57457528+e-chesoni@users.noreply.github.com>
2023-06-27 19:49:27 -0700

    laying groundwork for main page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image'
 import Head from 'next/head'
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import magneto from "../../public/magneto.jpeg";
+import bemo from "../../public/bemo.jpeg";
 
 export default function Home() {
   return (
@@ -17,10 +24,57 @@ export default function Home() {
               <li className='px-4'>Resume</li>
             </ul>
           </nav>
+          <div><p>Begin /&gt;</p></div>
+          <div>
+            <h2>Hi, my name is Elaine Chesoni</h2>
+            <div className='flex space-x-1'>
+              <p>I design and develop</p><p>Embedded Systems</p>
+            </div>
+          </div>
+          <div><p>Let me show you...</p></div>
         </section>
-        
+          
         <section>
+          <h1>Controls</h1>
+          <h2>Magneto</h2>
+          <div>
+            <h5>What?</h5>
+            <p>Pokem ipsum dolor sit amet Poison Sting Misty Raichu Elekid make it double Mineral Badge.</p>
+            <h5>How?</h5>
+            <p>Ice Klinklang S.S. Anne Palkia grumpy old man who needs coffee Igglybuff the enemy Pokemon fainted.</p>
+            <h5>Why?</h5>
+            <p>S.S. Anne Dig Ash Ketchum Skarmory Cryogonal Weedle Snivy.</p>
+          </div>
+          <div>
+            <Image 
+              className="rounded-lg object-cover"
+              src={magneto} 
+              alt={''}/>
+          </div>
+        </section>
 
+        <section>
+          <h1>Embedded Systems</h1>
+          <h2>Bem-0</h2>
+          <div>
+            <h5>What?</h5>
+            <p>Hydro Pump you're not wearing shorts Blizzard Slowking searching far and wide Burnt Berry Ninetales.</p>
+            <h5>How?</h5>
+            <p>Boulder Badge Grumpig Rotom Hippowdon Ninjask Flamethrower Muk.</p>
+            <h5>Why?</h5>
+            <p>Misty Pachirisu Dragon Zangoose Skorupi Armaldo Manectric.</p>
+          </div>
+          <div>
+            <Image 
+              className="rounded-lg object-cover"
+              src={bemo} 
+              alt={''}/>
+          </div>
+        </section>
+        <section className='text-5xl flex justify-center gap-16 py-3'>
+          <AiFillGithub />
+          <AiFillLinkedin />
+          <AiFillYoutube />
         </section>
       
       </main>
