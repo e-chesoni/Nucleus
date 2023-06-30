5667d0e0cc85e2927abc8e9bf6f0a8bf11b0986e
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-06-29 22:39:58 -0700

    Updating Main Background Color
    Now matching figma colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Head>
         <title>Elaine Chesoni Portfolio</title>
       </Head>
-      <main className='bg-gradient-to-b from-slate-900 to-orange-950'>
+      <main className='main-bg '>
         <NavBar/>
 
         {/* <Projects/> */}
