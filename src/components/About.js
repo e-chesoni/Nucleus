6e10b564eb573f0658529f638e1a35f5fa7f4a20
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-06-30 16:58:20 -0700

    Added Skills Section
    Progress bars using bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,130 @@
 "use client";
 
+import { Container, Col, Row } from "react-bootstrap";
+import { Skill } from "./Milestone";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 export const About = () => {
+
+    const skills = [
+        {
+            type: 'language',
+            skill: 'C#',
+        },
+        {
+            type: 'language',
+            skill: 'C++',
+        },
+        {
+            type: 'language',
+            skill: 'C',
+        },
+        {
+            type: 'language',
+            skill: 'Python',
+        },
+        {
+            type: 'language',
+            skill: 'JavaScript',
+        },
+        {
+            type: 'language',
+            skill: 'Java',
+        },
+        {
+            type: 'tool',
+            skill: 'Solidworks',
+        },
+        {
+            type: 'tool',
+            skill: 'Fusion 360',
+        },
+        {
+            type: 'tool',
+            skill: 'Altium',
+        },
+    ]
+
+    const work = [
+        {
+            timeline: '2022-now',
+            title: 'Controls Eningeer at Tertuliano Lab (UPENN)',
+        },
+        {
+            timeline: '2020-2022',
+            title: 'Mechatronics Engineer at Lab2Fab',
+        },
+        {
+            timeline: '2019-2020',
+            title: 'Software Engineer Intern at Sage Intacct',
+        },
+        {
+            timeline: '2017-2018',
+            title: 'Software Engineer Intern at Adobe Inc',
+        },
+        {
+            timeline: '2015-2017',
+            title: 'Software Engineer Intern at Adobe Inc',
+        },
+    ]
+
+    const education = [
+        {
+            timeline: '2022-now',
+            degree: 'MSE in Robotics at the University of Pennsylvania',
+        },
+        {
+            timeline: '2014-2017',
+            degree: 'MBA at Santa Clara University',
+        },
+        {
+            timeline: '2010-2014',
+            degree: 'BA at Santa Clara University',
+        },
+    ]
+
     return (
-        <div></div>
+        <Container>
+            <Col>
+                <Row>
+                    <Col>C#</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>C++</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>C</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>Python</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>JavaScript</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>Java</Col>
+                    <Col>
+                        <ProgressBar animated now={60} label={`${60}%`} variant="custom-progress-bar" />
+                    </Col>
+                </Row>
+                <Row>R1</Row>
+                <Row>R1</Row>
+            </Col>
+        </Container>
     )
 }
---
+++ src/components/Milestone.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { Col } from "react-bootstrap";
+
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+function Skill() {
+  return <ProgressBar animated now={45} />;
+}
+export default Skill;
